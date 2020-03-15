61245fbee0ccb71c0ae75f4f099ae11924673ea9
teratorn <epiphany-editor /AT/ teratorn /DOT/ net>
2020-03-14 23:28:20 -0500

    clean up locating of tiles.tga

--- setup.py
+++ setup.py
@@ -5,7 +5,15 @@ setup(name="epiphany-editor",
       description="Map Editor for Epiphany (a Boulder-Dash clone).",
       author="teratorn",
       author_email="epiphany-editor /AT/ teratorn /DOT/ net",
+      url='https://github.org/teratorn/epiphany-editor',
+      license='MIT',
       scripts=['epiphany-editor'],
-      scripts=['epiphany-editor'],
-      install_requires=['pygame', 'PyGObject']
+      install_requires=['pygame', 'PyGObject'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: X11 Applications :: GTK',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3 :: Only',
+      ]
       )
